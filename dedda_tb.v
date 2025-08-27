48ffa41e3b129abebbc2bb46e7ee1018c876fd18
Sharadhi-kedlaya <moonofautumn09@gmail.com>
2025-08-27 19:22:45 +0530

    Update dedda_tb.v

--- dedda_tb.v
+++ dedda_tb.v
@@ -1,26 +1,5 @@
 `timescale 1ns / 1ps
 
-////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer:
-//
-// Create Date:   12:58:58 06/17/2019
-// Design Name:   Dedda_mul
-// Module Name:   D:/xilinx_simulation/blog_mul/dedda_tb.v
-// Project Name:  blog_mul
-// Target Device:  
-// Tool versions:  
-// Description: 
-//
-// Verilog Test Fixture created by ISE for module: Dedda_mul
-//
-// Dependencies:
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-////////////////////////////////////////////////////////////////////////////////
 
 module dedda_tb;
 
@@ -71,4 +50,5 @@ module dedda_tb;
 	end
       
 endmodule
+
 
