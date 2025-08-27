be5a00c38fedfb45b1bf343a25cd7b7bd7231bf6
Sharadhi-kedlaya <moonofautumn09@gmail.com>
2025-08-27 19:14:07 +0530

    Update FA.v

--- FA.v
+++ FA.v
@@ -1,27 +1,8 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    20:29:56 01/29/2019 
-// Design Name: 
-// Module Name:    FA 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
-module FA(input a,b,c,
-          output s,co
-    );
+
+module FA(input a,b,c, output s,co );
 assign s = a^b^c;
 assign co = (a&b) | (b&c) | (c&a);
 
 endmodule
+
